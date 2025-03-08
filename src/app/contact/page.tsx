acef81d955c01df05a0299f0a75a9334b0bcb787
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-03-08 14:17:12 +0530

    contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,10 @@
 export default function Contact() {
-  const imageUrl =
-    "https://ptsinabocgqagjnqykgy.supabase.co/storage/v1/object/public/pic%20upload//Devil%20TF.jpg"; // Replace with your actual URL
 
   return (
     <div className="p-5">
       <h1 className="text-2xl font-bold mb-4">Contact Page</h1>
       <p>Here is the uploaded image:</p>
-      <img src={imageUrl} alt="Uploaded" className="w-40 h-40 object-cover border rounded" />
+      {/* <img src={imageUrl} alt="Uploaded" className="w-40 h-40 object-cover border rounded" /> */}
     </div>
   );
 }
